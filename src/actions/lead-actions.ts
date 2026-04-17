0b1dc2fbecb5c19a9d8d69ace7864add1981c2b4
MAVIE-CRM <support@itsmavie.it>
2026-04-17 20:32:29 +0200

    Add missing cookies import

--- src/actions/lead-actions.ts
+++ src/actions/lead-actions.ts
@@ -2,6 +2,7 @@
 
 import prisma from "@/lib/prisma"
 import { revalidatePath } from 'next/cache'
+import { cookies } from 'next/headers'
 import { createActivity } from './lead-detail'
 import { createCalendarEvent } from './calendar'
 import { getCurrentUser } from './auth'
